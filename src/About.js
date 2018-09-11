faea945d8726a8adad49062f7f33414e5a640a06
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-11 13:11:25 +0100

    feat: Add contact information

--- src/About.js
+++ src/About.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { people } from './data';
 
 class About extends Component {
   render() {
     return (
       <main>
         <h1>About Us</h1>
+        {people.map(person => <Person {...person} />)}
       </main>
     );
   }
 }
+
+const Person = ({name, position, email, about, img}) => (
+    <div class="person">
+    <h2>{name} - {position}</h2>
+    <img src={`./img/${img}`} />
+    <address><a href={`mailto:${email}`}>{email}</a></address>
+    <div class="description">{about}</div>
+    </div>
+)
+
 
 export default About;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const people = [
+  {
+    name: 'Jennifer Dyer',
+    position: 'CEO',
+    email: 'jennifer@yourhealth.com',
+    about: 'Jennifer started work in 2017 and immediately became CEO because she\'s awesome. Her ambition is to take over the world.',
+    img: 'jennifer.png'
+  },
+  {
+    name: 'Bob Hoskins',
+    position: 'Lead Psychiatrist ',
+    email: 'bob@yourheath.com',
+    about: 'Bob is our lead psychiatrist. He will fix your brain while you wait.',
+    img: 'bob.png'
+  },
+  {
+    name: 'Fern',
+    position: 'Head of Recruitment',
+    email: 'fern@yourhealth.com',
+    about: 'Fern has been recruiting for over 100 years and is unchallenged in her field.',
+    img: 'fern.png'
+  }
+];
