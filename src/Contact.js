7ee08db6c1b75a7c088882f0d43162354ccda245
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-11 13:57:31 +0100

    feat: Add contact form to contact page

--- src/Contact.js
+++ src/Contact.js
@@ -5,6 +5,7 @@ class Contact extends Component {
     return (
       <main>
         <h1>Contact Us</h1>
+        <ContactForm />
         <address>
           YourHealth Ltd.<br />
           Common Rd,<br />
@@ -16,5 +17,28 @@ class Contact extends Component {
     );
   }
 }
+
+const ContactForm = () => (
+<form>
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" />
+  </div>
+  <div>
+    <label for="email">E-mail:</label>
+    <input type="email" id="email" name="email" />
+  </div>
+  <div>
+    <label for="message">Message:</label>
+    <textarea id="message" name="message"></textarea>
+  </div>
+  <div id="button">
+    {/*preventDefault() called to stop form from submitting*/}
+    <button type="submit" onClick={e => e.preventDefault()}>
+      Send
+    </button>
+  </div>
+</form>
+)
 
 export default Contact;
