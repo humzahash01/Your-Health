de9fb9a4e6c4bfb9ca95f2b38471299fb83057a6
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-11 15:45:19 +0100

    feat: Add Google Maps to the Contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  InfoWindow,
+  GoogleMap,
+  Marker,
+  withGoogleMap,
+  withScriptjs
+} from 'react-google-maps';
 
 class Contact extends Component {
   render() {
@@ -6,39 +13,64 @@ class Contact extends Component {
       <main>
         <h1>Contact Us</h1>
         <ContactForm />
-        <address>
-          YourHealth Ltd.<br />
-          Common Rd,<br />
-          Tadcaster<br />
-          LS24 9UN,<br />
-          UK<br />
-        </address>
+        <Map
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </main>
     );
   }
 }
 
 const ContactForm = () => (
-<form>
-  <div>
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" />
-  </div>
-  <div>
-    <label for="email">E-mail:</label>
-    <input type="email" id="email" name="email" />
-  </div>
-  <div>
-    <label for="message">Message:</label>
-    <textarea id="message" name="message"></textarea>
-  </div>
-  <div id="button">
-    {/*preventDefault() called to stop form from submitting*/}
-    <button type="submit" onClick={e => e.preventDefault()}>
-      Send
-    </button>
-  </div>
-</form>
-)
+  <form>
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" />
+    </div>
+    <div>
+      <label for="email">E-mail:</label>
+      <input type="email" id="email" name="email" />
+    </div>
+    <div>
+      <label for="message">Message:</label>
+      <textarea id="message" name="message" />
+    </div>
+    <div id="button">
+      {/*preventDefault() called to stop form from submitting*/}
+      <button type="submit" onClick={e => e.preventDefault()}>
+        Send
+      </button>
+    </div>
+  </form>
+);
+
+const Map = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 53.82048, lng: -1.25199 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: 53.82048, lng: -1.25199 }}>
+          <InfoWindow>
+            <address>
+              YourHealth Ltd.
+              <br />
+              Common Rd,
+              <br />
+              Tadcaster
+              <br />
+              LS24 9UN,
+              <br />
+              UK
+              <br />
+            </address>
+          </InfoWindow>
+        </Marker>
+      )}
+    </GoogleMap>
+  ))
+);
 
 export default Contact;
