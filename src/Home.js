2aff10004fdc619fd63dd14946dab45af6b466e8
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-11 13:18:38 +0100

    feat: Add copy to home page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,13 @@ class Home extends Component {
   render() {
     return (
       <main>
-        <h1>Home page</h1>
+        <h1>Your Health</h1>
+        <h2>== HERE FOR YOU ==</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit erat ac ipsum accumsan pulvinar. Etiam tortor lorem, mollis eget nibh tempor, mattis accumsan lectus. Sed in consequat ligula. Curabitur sed purus lacus. Nulla pretium tincidunt aliquet. Etiam nisi sapien, faucibus non nulla in, pharetra luctus ex. In eget tristique dui, euismod luctus arcu. Etiam imperdiet et dui bibendum pharetra. Curabitur id mauris lacus. Suspendisse potenti. Cras finibus libero a quam mattis, eu varius leo auctor. Maecenas nisi lacus, vestibulum sed tincidunt sodales, dictum at turpis. Pellentesque nec lorem vitae justo blandit pretium vel et lectus. Suspendisse aliquam vel ligula sit amet sagittis. Pellentesque lobortis diam vel magna venenatis, vel laoreet ex fermentum.</p>
+        <h2>== MAKING THE WORLD A HEALTHIER PLACE ==</h2>
+        <p>Donec tincidunt nec erat ut tincidunt. Integer non sem nec urna venenatis feugiat. Curabitur ut est sit amet justo ullamcorper posuere a non ligula. Etiam in ex quis velit porttitor euismod. Suspendisse volutpat blandit sodales. Sed imperdiet feugiat tortor sed vestibulum. Curabitur dignissim fermentum ex, non malesuada tellus iaculis quis. Vivamus tempus lorem sit amet laoreet lobortis.</p>
+        <h2>== MEDICATION'S WHAT YOU NEED ==</h2>
+        <p>Sed placerat accumsan tellus a viverra. Aliquam aliquam cursus ligula, sit amet aliquam metus condimentum et. Etiam sed nisi nec est venenatis accumsan. Duis tristique, turpis non tincidunt sagittis, sapien sem tempor nibh, ut posuere tortor sem luctus eros. Duis egestas erat ac eleifend commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce tincidunt, lectus eget porttitor tempus, lorem erat mollis lacus, id imperdiet lacus diam quis dolor.</p>
       </main>
     );
   }
