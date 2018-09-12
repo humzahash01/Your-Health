6ac3201e3e53c776ca25f955cea9fed956f0ac85
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-12 12:28:41 +0100

    feat: Move data from Home.js
    Storing the data in the data.js file seperates the view and data layer
    and also allow for easier maintainence of the code.

--- src/Home.js
+++ src/Home.js
@@ -1,52 +1,27 @@
 import React, { Component } from 'react';
+import { homeArticles } from './data';
 
 class Home extends Component {
   render() {
     return (
       <main>
         <img id="home-splash" src="./img/mental-health.png" />
-        <h1>YourHealth</h1>
-        <div id="home-content">
-          {' '}
-          <h2>== HERE FOR YOU ==</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            blandit erat ac ipsum accumsan pulvinar. Etiam tortor lorem, mollis
-            eget nibh tempor, mattis accumsan lectus. Sed in consequat ligula.
-            Curabitur sed purus lacus. Nulla pretium tincidunt aliquet. Etiam
-            nisi sapien, faucibus non nulla in, pharetra luctus ex. In eget
-            tristique dui, euismod luctus arcu. Etiam imperdiet et dui bibendum
-            pharetra. Curabitur id mauris lacus. Suspendisse potenti. Cras
-            finibus libero a quam mattis, eu varius leo auctor. Maecenas nisi
-            lacus, vestibulum sed tincidunt sodales, dictum at turpis.
-            Pellentesque nec lorem vitae justo blandit pretium vel et lectus.
-            Suspendisse aliquam vel ligula sit amet sagittis. Pellentesque
-            lobortis diam vel magna venenatis, vel laoreet ex fermentum.
-          </p>
-          <h2>== MAKING THE WORLD A HEALTHIER PLACE ==</h2>
-          <p>
-            Donec tincidunt nec erat ut tincidunt. Integer non sem nec urna
-            venenatis feugiat. Curabitur ut est sit amet justo ullamcorper
-            posuere a non ligula. Etiam in ex quis velit porttitor euismod.
-            Suspendisse volutpat blandit sodales. Sed imperdiet feugiat tortor
-            sed vestibulum. Curabitur dignissim fermentum ex, non malesuada
-            tellus iaculis quis. Vivamus tempus lorem sit amet laoreet lobortis.
-          </p>
-          <h2>== MEDICATION'S WHAT YOU NEED ==</h2>
-          <p>
-            Sed placerat accumsan tellus a viverra. Aliquam aliquam cursus
-            ligula, sit amet aliquam metus condimentum et. Etiam sed nisi nec
-            est venenatis accumsan. Duis tristique, turpis non tincidunt
-            sagittis, sapien sem tempor nibh, ut posuere tortor sem luctus eros.
-            Duis egestas erat ac eleifend commodo. Interdum et malesuada fames
-            ac ante ipsum primis in faucibus. Fusce tincidunt, lectus eget
-            porttitor tempus, lorem erat mollis lacus, id imperdiet lacus diam
-            quis dolor.
-          </p>
-        </div>
+        {homeArticles.map(article => (
+          <Article {...article} />
+        ))}
       </main>
     );
   }
 }
+
+const Article = ({ title, content, img }) => (
+  <article>
+    <img src={`./img/${img.name}`} alt={img.alt} />
+    <div>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  </article>
+);
 
 export default Home;
--- src/data.js
+++ src/data.js
@@ -3,21 +3,54 @@ export const people = [
     name: 'Jennifer Dyer',
     position: 'CEO',
     email: 'jennifer@yourhealth.com',
-    about: 'Jennifer started work in 2017 and immediately became CEO because she\'s awesome. Her ambition is to take over the world.',
+    about:
+      "Jennifer started work in 2017 and immediately became CEO because she's awesome. Her ambition is to take over the world.",
     img: 'jennifer.png'
   },
   {
     name: 'Bob Hoskins',
     position: 'Lead Psychiatrist ',
     email: 'bob@yourheath.com',
-    about: 'Bob is our lead psychiatrist. He will fix your brain while you wait.',
+    about:
+      'Bob is our lead psychiatrist. He will fix your brain while you wait.',
     img: 'bob.png'
   },
   {
     name: 'Fern',
     position: 'Head of Recruitment',
     email: 'fern@yourhealth.com',
-    about: 'Fern has been recruiting for over 100 years and is unchallenged in her field.',
+    about:
+      'Fern has been recruiting for over 100 years and is unchallenged in her field.',
     img: 'fern.png'
+  }
+];
+
+export const homeArticles = [
+  {
+    title: '== HERE FOR YOU ==',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit erat ac ipsum accumsan pulvinar. Etiam tortor lorem, mollis eget nibh tempor, mattis accumsan lectus. Sed in consequat ligula. Curabitur sed purus lacus. Nulla pretium tincidunt aliquet. Etiam nisi sapien, faucibus non nulla in, pharetra luctus ex. In eget tristique dui, euismod luctus arcu. Etiam imperdiet et dui bibendum pharetra. Curabitur id mauris lacus. Suspendisse potenti. Cras finibus libero a quam mattis, eu varius leo auctor. Maecenas nisi lacus, vestibulum sed tincidunt sodales, dictum at turpis. Pellentesque nec lorem vitae justo blandit pretium vel et lectus. Suspendisse aliquam vel ligula sit amet sagittis. Pellentesque lobortis diam vel magna venenatis, vel laoreet ex fermentum.',
+    img: {
+      name: 'stethoscope.jpg',
+      alt: 'stethoscope'
+    }
+  },
+  {
+    title: '== MAKING THE WORLD A HEALTHIER PLACE ==',
+    content:
+      'Donec tincidunt nec erat ut tincidunt. Integer non sem nec urna venenatis feugiat. Curabitur ut est sit amet justo ullamcorper posuere a non ligula. Etiam in ex quis velit porttitor euismod. Suspendisse volutpat blandit sodales. Sed imperdiet feugiat tortor sed vestibulum. Curabitur dignissim fermentum ex, non malesuada tellus iaculis quis. Vivamus tempus lorem sit amet laoreet lobortis.',
+    img: {
+      name: 'medic.jpg',
+      alt: 'medic'
+    }
+  },
+  {
+    title: '== MEDICATIONS THAT YOU NEED ==',
+    content:
+      'Sed placerat accumsan tellus a viverra. Aliquam aliquam cursus ligula, sit amet aliquam metus condimentum et. Etiam sed nisi nec est venenatis accumsan. Duis tristique, turpis non tincidunt sagittis, sapien sem tempor nibh, ut posuere tortor sem luctus eros. Duis egestas erat ac eleifend commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce tincidunt, lectus eget porttitor tempus, lorem erat mollis lacus, id imperdiet lacus diam quis dolor.',
+    img: {
+      name: 'dna.jpg',
+      alt: 'dna'
+    }
   }
 ];
