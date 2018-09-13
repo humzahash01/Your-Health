aa441422eb1ec580942b00cdc58cd0dec4a7618a
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-13 12:40:14 +0100

    style: Tidy and add comments to CSS

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
+          <img src={logo} className="logo" alt="logo" />
           <a id="toggle-link" href="#" onClick={toggleNav}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
               <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z" />
