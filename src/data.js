9b04ddbecfe01b3a2ac13001e6e6738f7bc6f666
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-09-13 10:06:34 +0100

    chore: Change capitalisation of article titles

--- src/data.js
+++ src/data.js
@@ -27,7 +27,7 @@ export const people = [
 
 export const homeArticles = [
   {
-    title: '== HERE FOR YOU ==',
+    title: 'Here For You',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit erat ac ipsum accumsan pulvinar. Etiam tortor lorem, mollis eget nibh tempor, mattis accumsan lectus. Sed in consequat ligula. Curabitur sed purus lacus. Nulla pretium tincidunt aliquet. Etiam nisi sapien, faucibus non nulla in, pharetra luctus ex. In eget tristique dui, euismod luctus arcu. Etiam imperdiet et dui bibendum pharetra. Curabitur id mauris lacus. Suspendisse potenti. Cras finibus libero a quam mattis, eu varius leo auctor. Maecenas nisi lacus, vestibulum sed tincidunt sodales, dictum at turpis. Pellentesque nec lorem vitae justo blandit pretium vel et lectus. Suspendisse aliquam vel ligula sit amet sagittis. Pellentesque lobortis diam vel magna venenatis, vel laoreet ex fermentum.',
     img: {
@@ -36,7 +36,7 @@ export const homeArticles = [
     }
   },
   {
-    title: '== MAKING THE WORLD A HEALTHIER PLACE ==',
+    title: 'Making The World A Healthier Place',
     content:
       'Donec tincidunt nec erat ut tincidunt. Integer non sem nec urna venenatis feugiat. Curabitur ut est sit amet justo ullamcorper posuere a non ligula. Etiam in ex quis velit porttitor euismod. Suspendisse volutpat blandit sodales. Sed imperdiet feugiat tortor sed vestibulum. Curabitur dignissim fermentum ex, non malesuada tellus iaculis quis. Vivamus tempus lorem sit amet laoreet lobortis.',
     img: {
@@ -45,7 +45,7 @@ export const homeArticles = [
     }
   },
   {
-    title: '== MEDICATIONS THAT YOU NEED ==',
+    title: 'Medication\'s What You Need',
     content:
       'Sed placerat accumsan tellus a viverra. Aliquam aliquam cursus ligula, sit amet aliquam metus condimentum et. Etiam sed nisi nec est venenatis accumsan. Duis tristique, turpis non tincidunt sagittis, sapien sem tempor nibh, ut posuere tortor sem luctus eros. Duis egestas erat ac eleifend commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce tincidunt, lectus eget porttitor tempus, lorem erat mollis lacus, id imperdiet lacus diam quis dolor.',
     img: {
